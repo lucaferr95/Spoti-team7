b67cced2a39ef0cc68e9301cad41ced6e40a911d
pandino126 <re.luca95@gmail.com>
2025-03-24 19:03:15 +0100

    aggiustare css album

--- assets/JS/albumpage.js
+++ assets/JS/albumpage.js
@@ -18,38 +18,41 @@ fetch(gioventuBruciata, {
   })
   .then((data) => {
     console.log("DATI RICEVUTI DAL SERVER", data);
+    displayAlbum(data);
   })
   .catch((error) => {
     console.log("Error:", error);
   });
 
 const displayAlbum = (data) => {
-  //prendo un riferimento alla row definita in HTML per la parte superiore con la foto dell'ALBUM
   const row = document.getElementById("displayAlbum");
   row.innerHTML = `
-    <div class="album-header d-flex align-items-center p-4">
-        <img src="${data.cover_big}" class="album-cover me-3" alt="${
+      <div class="album-header d-flex align-items-center p-4">
+          <img src="${data.cover_big}" class="album-cover me-3" alt="${
     data.title
   }">
-        <div>
-            <h2>${data.title}</h2>
-            <ul class="list-group">
-                ${data.tracks.data
-                  .map(
-                    (track, index) => `
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <span>${index + 1}. ${track.title}</span>
-                        <span>${Math.floor(track.duration / 60)}:${(
-                      track.duration % 60
+          <div>
+              <h2>${data.title}</h2>
+              <ul class="list-group">
+                  ${data.tracks.data
+                    .map(
+                      (track, index) => `
+                      <li class="list-group-item w-100 text-white d-flex justify-content-between align-items-center"
+                          style="background-color: #21160F; border: none;">
+                          <span>${index + 1}. ${track.title}</span>
+                          <span>${Math.floor(track.duration / 60)}:${(
+                        track.duration % 60
+                      )
+                        .toString()
+                        .padStart(2, "0")}</span>
+                      </li>
+                  `
                     )
-                      .toString()
-                      .padStart(2, "0")}</span>
-                    </li>
-                `
-                  )
-                  .join("")}
-            </ul>
-        </div>
-    </div>
-`;
+                    .join("")}
+              </ul>
+          </div>
+      </div>
+    `;
 };
+
+displayAlbum();
