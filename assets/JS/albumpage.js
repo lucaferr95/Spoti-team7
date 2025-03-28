234ea7e27356a5cda96f8674671d8b7bc8f057f0
Vincenza <vinni2690@hotmail.com>
2025-03-28 11:03:48 +0100

    responsive in every page

--- assets/JS/albumpage.js
+++ assets/JS/albumpage.js
@@ -47,7 +47,7 @@ const displayAlbum = (data) => {
           }" class="album-cover img-fluid py-5 ps-3" alt="${data.title}">
         </div>
         <div class="col-md-8 d-flex flex-column">
-          <h3 class="text-center text-black text-gradient fs-2"> NOW PLAYING </h3>
+          <h3 class="text-black text-gradient fs-2"> NOW PLAYING </h3>
           <div class="album-title-container">
             <h2 class="text-decoration-underline text-warning fw-bold text-opacity-75 display-4 album-font album-title fs-5 text-wrap">
               ${data.title}
